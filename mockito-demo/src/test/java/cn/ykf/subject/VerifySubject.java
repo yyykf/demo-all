14d9de6095b542d4b5bcadb5b22c15369cfade4a
yukaifan <yukf@pvc123.com>
2021-05-07 10:48:59 +0800

    feat(): about verify usage of mockito

---
+++ mockito-demo/src/test/java/cn/ykf/subject/VerifySubject.java
@@ -0,0 +1,32 @@
+package cn.ykf.subject;
+
+/**
+ * verify subject
+ *
+ * @author YuKaiFan <yukf@pvc123.com>
+ * @date 2021/5/7
+ */
+public class VerifySubject {
+
+    public void methodA() {
+        System.out.println("method a...");
+    }
+
+    public void methodB(int i) {
+        int key = i % 2;
+
+        switch (key) {
+            case 0:
+                methodA();
+                break;
+            case 1:
+                methodC();
+                break;
+        }
+    }
+
+    public void methodC() {
+        System.out.println("method c...");
+    }
+
+}
